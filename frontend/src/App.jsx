cc12402f2617aa07c6dbfbb43359f83c8087dce0
HarshitaShakya <shakyaharshita20@gmail.com>
2026-07-12 05:00:37 +0530

    Update frontend application routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,18 @@ import Auth from './pages/Auth'
 import Favourites from './pages/Favourites'
 import FreeServicesResults from './pages/FreeServicesResults'
 import AiAnalysis from './pages/AiAnalysis'
+import InfoPage from './pages/InfoPage'
+import HelpCenter from './pages/HelpCenter'
+import Faqs from './pages/Faqs'
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import TermsOfUse from './pages/TermsOfUse'
+import ContactUs from './pages/ContactUs'
+import AboutUs from './pages/AboutUs'
+import OurMission from './pages/OurMission'
+import Careers from './pages/Careers'
+import MediaKit from './pages/MediaKit'
+import Features from './pages/Features'
+import CommunityReviews from './pages/CommunityReviews'
 
 function App() {
   const darkMode = useAppStore(s => s.darkMode)
@@ -21,9 +33,21 @@ function App() {
           <Route path="/search" element={<SearchResults />} />
           <Route path="/free-services-search" element={<FreeServicesResults />} />
           <Route path="/hospital/:id" element={<HospitalProfile />} />
+          <Route path="/features" element={<Features />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/favourites" element={<Favourites />} />
           <Route path="/ai-analysis" element={<AiAnalysis />} />
+          <Route path="/info/faqs" element={<Faqs />} />
+          <Route path="/info/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/info/terms-of-use" element={<TermsOfUse />} />
+          <Route path="/info/contact-us" element={<ContactUs />} />
+          <Route path="/info/about-us" element={<AboutUs />} />
+          <Route path="/info/our-mission" element={<OurMission />} />
+          <Route path="/info/careers" element={<Careers />} />
+          <Route path="/info/media-kit" element={<MediaKit />} />
+          <Route path="/info/community-reviews" element={<CommunityReviews />} />
+          <Route path="/info/:pageId" element={<InfoPage />} />
+          <Route path="/help-center" element={<HelpCenter />} />
         </Routes>
         <AIChat />
       </div>
